693e665304852c62b32be53e32d67aaac9799561
broezer <bruce@moerdjiman.com>
2021-01-04 20:16:09 +0100

    yay, getting less hacky on creating the shapes with the formulas, still improvement nedded

---
+++ README.md
@@ -0,0 +1,6 @@
+Thus far I have attempted to create gradients for primitive shapes like squares, triangles, circles.
+To create these gradients I have to draw lines or arc within a for loop to achieve this. Basically we trick the computer by drawing over the canvas different shapes.
+Using the interpolation color function (lerp) I can easily set a beginning and end point.
+Also to place the object in the right spot on the canvas I use the translate function. 
+I have to wrap my head around all these steps and think differently about creating graphics.
+Fun times, wohoo!
---
+++ p5js-temp-sketch_210104a5372285387866333247.js
@@ -0,0 +1,57 @@
+// Constants
+const Y_AXIS = 1;
+const X_AXIS = 2;
+let c1, c2;
+
+
+function setup() {
+  createCanvas(400, 400);
+  c1 = color(111, 84, 8);
+  c2 = color(242, 207, 110);
+}
+
+
+function draw() {
+  //Background;
+  setGradient(0, 0, width, height, c2, c1, Y_AXIS);
+  
+  //Diamond
+  translate(width/2, height*0.125);
+  grdDiamond(0, 0, height*0.75, c2, c1);
+}
+
+function grdDiamond(x, y, h, c1, c2){
+  for (let i = y; i <= y + h; i++) {
+      let inter = map(i, y, y + h, 0, 1);
+      let c = lerpColor(c2, c1, inter);
+      stroke(c);
+      if ( i <= ((y + h)/2)){
+        line( (x * h) - (i), i, (x * h) + i, i);
+      }else{
+        line( (x - h) + i , i, (x + h) - i , i);
+      }
+    }
+}
+
+
+function setGradient(x, y, w, h, c1, c2, axis) {
+  noFill();
+
+  if (axis === Y_AXIS) {
+    // Top to bottom gradient
+    for (let i = y; i <= y + h; i++) {
+      let inter = map(i, y, y + h, 0, 1);
+      let c = lerpColor(c1, c2, inter);
+      stroke(c);
+      line(x, i, x + w, i);
+    }
+  } else if (axis === X_AXIS) {
+    // Left to right gradient
+    for (let i = x; i <= x + w; i++) {
+      let inter = map(i, x, x + w, 0, 1);
+      let c = lerpColor(c1, c2, inter);
+      stroke(c);
+      line(i, y, i, y + h);
+    }
+  }
+}
